fa002737b27977cbd50e9fc4ccfce2504492db91
edidiesky <essienedidiong1000@gmail.com>
2025-03-21 06:02:57 -0700

    fix(users-endpoint): Disconecting socket handler

--- src/sockets/message.socket.ts
+++ src/sockets/message.socket.ts
@@ -18,27 +18,28 @@ export default function setupMessageSocket(io: Server) {
     socket.on("send_dm", async (data) => {
       const { toUserId, body, userId, subject, conversationId } = data;
       const toSockerUserId = await redisClient.get(`active_users:${toUserId}`);
+      const messageservice = new MessageService();
+      let message = await messageservice.createMessageService(
+        toUserId,
+        body,
+        userId,
+        subject,
+        conversationId
+      );
       if (toSockerUserId) {
-        const messageservice = new MessageService();
-        let message = await messageservice.createMessageService(
-          toUserId,
-          body,
-          userId,
-          subject,
-          conversationId
-        );
         // create the message from db
-        io.to(toSockerUserId).emit("receive_dm", message);
+        io.to(`user:${toSockerUserId}`).emit("receive_dm", message);
       }
+      io.to(`user:${userId}`).emit("receive_dm", message);
     });
 
     // Start Typing Event
     socket.on("user_typing", async (data) => {
       const { toUserId } = data;
-      const toSockerUserId = await redisClient.get(toUserId);
+      const toSockerUserId = await redisClient.get(`active_users:${toUserId}`);
 
       if (toSockerUserId) {
-        io.to(toSockerUserId).emit("user_typing", { user: data.userId });
+        io.to(`user:${toUserId}`).emit("user_typing", { user: data.userId });
       }
     });
 
@@ -48,9 +49,34 @@ export default function setupMessageSocket(io: Server) {
       const toSockerUserId = await redisClient.get(toUserId);
 
       if (toSockerUserId) {
-        io.to(toSockerUserId).emit("user_stopped_typing", {
+        io.to(`user:${toSockerUserId}`).emit("user_stopped_typing", {
           user: data.userId,
         });
+      }
+    });
+    /**
+     * @description  Disconnecting a user from the channel
+     */
+    socket.on("disconnect", async () => {
+      try {
+        const redisKeys = await redisClient.keys(`active_users:*`);
+
+        /**
+         * get the key that matched the user user id
+         */
+        const userKey = await Promise.all(
+          redisKeys.map(async (key) => {
+            const value = await redisClient.get(key);
+            return value === socket.id ? key : null;
+          })
+        ).then((resultKey) => resultKey.find((key) => key !== null));
+        const userId = userKey?.split(":")[1];
+        if (userId) {
+          // delete the user's key
+          await redisClient.del(`active_users:${userId}`);
+        }
+      } catch (error) {
+        console.log(`Disconnection error`, error);
       }
     });
   });
